7a49cce752d67ec997db282a9f8f65d303c5da6e
johanvax <152924301+johanvax@users.noreply.github.com>
2023-12-05 12:52:03 +0100

    start day3

---
+++ Day_3/day3.py
@@ -0,0 +1,8 @@
+# Search for digits, and concatenate for the whole number
+# check if any of the numbers positions are close to a symbol
+# if they are, add the number to the result
+
+grid = [][]
+
+with open('day3test.txt') as f:
+    lines = f.readlines()
