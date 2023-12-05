2b3c7de7a37f27d0dfd84c367b9df2c2a39ba602
johanvax <152924301+johanvax@users.noreply.github.com>
2023-12-05 11:40:16 +0100

    day 2 done

--- Day_2/day2-2.py
+++ Day_2/day2-2.py
@@ -1,24 +1,36 @@
 import re
 
-with open('day2test.txt') as f:
+with open('day2input.txt') as f:
     lines = f.readlines()
 
 result = 0
 for line in lines:
-    
+    subsets = []
+    redvals = []
+    greenvals = []
+    bluevals = []
     line = line.strip()
     line = line.replace('\n', '')
     line = re.split(':', line)
-    subsets = re.split(';', line[1])
-    for i in range(0, len(subsets)):
-        instance = re.split(',', subsets[i])
+    temp = re.split(';', line[1])
+    for i in range(0, len(temp)):
+        instance = re.split(',', temp[i])
         for i in range(0, len(instance)):
             instance[i] = instance[i].strip()
             instance[i] = re.split(' ', instance[i])
-        print(instance)
-            
+            subsets.append(instance[i])
 
-        
+    for i in range(0, len(subsets)):
+        if subsets[i][1] == 'green':
+            redvals.append(int(subsets[i][0]))
+        elif subsets[i][1] == 'blue':
+            bluevals.append(int(subsets[i][0]))
+        else:
+            greenvals.append(int(subsets[i][0]))
+
+    result += max(greenvals)*max(redvals)*max(bluevals)
+    
+              
 print(result)
 
     
