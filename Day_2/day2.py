7b20a358740a842ace1b70e4685868b732658968
johanvax <152924301+johanvax@users.noreply.github.com>
2023-12-05 09:13:31 +0100

    day2p1 complete

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -1,10 +1,31 @@
 import re
 
-colors = ['red', 'green', 'blue']
+colors = {'red' : 12, 'green' : 13, 'blue' : 14}
 
-with open('day2test.txt') as f:
+with open('day2input.txt') as f:
     lines = f.readlines()
 
+id = 1
+result = 0
 for line in lines:
-    line = re.split(': | ;')
-    print(line)
+    temp = True
+    line = line.strip()
+    line = line.replace('\n', '')
+    line = re.split(':', line)
+    subsets = re.split(';', line[1])
+    for i in range(0, len(subsets)):
+        instance = re.split(',', subsets[i])
+        for i in range(0, len(instance)):
+            instance[i] = instance[i].strip()
+            instance[i] = re.split(' ', instance[i])
+            for color in colors:
+                if instance[i][1] == color and int(instance[i][0]) > colors[color]:
+                    temp = False
+
+    if temp:
+        result += id
+        
+    id = id + 1
+print(result)
+
+    
